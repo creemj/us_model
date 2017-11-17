084ed1c5587de62e3d218ccac7f1a6bbc8742c91
creemj <jonathan.creem1@gmail.com>
2017-11-16 21:05:16 -0500

    Add files via upload

---
+++ us_model/us_model_v1.R
@@ -0,0 +1,10 @@
+#Output
+#Unemployment
+#Inflation
+#Real Interest
+
+
+install.packages("vars")
+library("vars")
+us_model=read.csv('/Users/jcreem/Desktop/Economics/VAR/us_model/us_model.csv')
+summary(us_model)
